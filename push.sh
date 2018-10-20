11c11356006a1f9b94c3cc72be6f720d86c0583b
sagarpinninti <sagar.pinninti@gmail.com>
2018-10-20 22:11:49 +0530

    2018-10-202211

---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+t="$(date '+%F%H%M')"
+git remote add origin git@github.com:sagarpinninti/test.git
+git add *;
+git commit -am $t;
+git push origin master
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+git pull
+update a file 
+commit 
+git push 
+sequence testing 
