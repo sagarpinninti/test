057cbd4fecdcd73d7782418dbf5572c6ef1a2e50
sagar Pinninti <31984052+sagarpinninti@users.noreply.github.com>
2018-10-20 22:29:44 +0530

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 t="$(date '+%F%H%M')"
-git remote add origin git@github.com:sagarpinninti/test.git
+git remote set-url origin git@github.com:sagarpinninti/test.git;
+git remote add origin git@github.com:sagarpinninti/test.git;
 git add *;
 git commit -am $t;
 git push origin master --force
